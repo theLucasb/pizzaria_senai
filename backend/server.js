65990ce506d49ff3153c284cdebc98ccf0b30b87
theLucasb <lucasrodrigodes@gmail.com>
2026-07-28 14:54:01 -0300

    Configuração para o render

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
